e75422ee0f7f5939d1c335a67318b36625d23d51
QlexanderPC <598926542@qq.com>
2024-10-12 15:53:10 +0800

    Complete all features

--- Markdown-Image-Upload.py
+++ Markdown-Image-Upload.py
@@ -1,24 +1,76 @@
 import re
-import os
-import shutil
+# import os
+# import shutil
 import sys
+import requests
 
-markdown_file = sys.argv[1]
-with open(markdown_file, 'r', encoding='utf-8') as f:
-    content = f.read()
+def get_local_img(markdown_file):
+    with open(markdown_file, 'r', encoding='utf-8') as f:
+        content = f.read()
 
-image_pattern = r'!\[.*?\]\((.*?)\)'
-image_paths = re.findall(image_pattern, content)
-current_directory = os.getcwd()
+    image_pattern = r'!\[.*?\]\((.*?)\)'
+    image_local_path = re.findall(image_pattern, content)
+    # current_directory = os.getcwd()
 
-for image_path in image_paths:
-    if os.path.exists(image_path):
-        image_name = os.path.basename(image_path)
-        
-        destination = os.path.join(current_directory, 'result', image_name)
-        shutil.copy(image_path, destination)
-        print(f"已将图片 {image_name} 复制到当前目录")
+    return image_local_path
+
+
+def get_network_img(image_local, token):
+    upload_url = 'https://smms.app/api/v2/upload'
+    headers = {
+        'Authorization' : token
+    }
+
+    with open(image_local, 'rb') as img_file:
+        files = {'smfile': img_file}
+        response = requests.post(upload_url, files=files, headers=headers)
+    
+    if response.status_code == 200:
+        res_data = response.json()
+        if res_data['success']:                                     # 上传成功
+            return res_data['data']['url']
+        elif "Image upload repeated limit" in res_data['message']:  # 重复上传
+            return res_data['images']
+        else:                                                       # 上传失败
+            print(f"上传失败: {res_data['message']}")
     else:
-        print(f"图片路径 {image_path} 不存在或不是本地路径")
+        print(f"HTTP 请求错误: {response.status_code}")
+    return None
 
-print("图片处理完成。")
+
+def replace_img(markdown_file, img_local_path, img_network_path):
+    with open(markdown_file, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    for i in range(len(img_local_path)):
+        content = content.replace(img_local_path[i], img_network_path[i])
+
+    with open(markdown_file, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
+# for image_path in image_local:
+#     if os.path.exists(image_local):
+#         image_name = os.path.basename(image_local)
+        
+#         destination = os.path.join(current_directory, 'result', image_name)
+#         shutil.copy(image_local, destination)
+#         print(f"已将图片 {image_name} 复制到当前目录")
+#     else:
+#         print(f"图片路径 {image_local} 不存在或不是本地路径")
+
+# print("图片处理完成。")
+
+
+if __name__ == '__main__':
+    markdown_file = sys.argv[1]
+    authorization = sys.argv[2]
+
+    img_local_path = get_local_img(markdown_file)
+    img_network_path = []
+
+    for img_path in img_local_path:
+        img_network_path.append(get_network_img(img_path, authorization))
+
+    replace_img(markdown_file, img_local_path, img_network_path)
+    print("图片处理完成。")
--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # Introduction
 
-本项目用于将 markdown 文件中的本地图片全部替换为将图片上传托管网站后的网络图片地址。
+本项目用于将 markdown 文件中的本地图片全部替换为将图片上传托管网站后的网络图片地址，是一个简单的自动化程序。
 
 写 markdown 的时候常常会随手贴一张本地图片，但在将 markdown 文件直接分享给他人时别人是看不见你本地的图片的。本项目旨在将你 markdown 中的本地图片一键替换为网络图片，这样所有人查看你的 markdown 文件时都不用再为此烦恼。
+
+本项目采用的图片托管网站为 https://smms.app/，参考了 https://doc.sm.ms/
 
 # Status
 
 - [X] 获取 markdown 文件中的本地图片地址
-- [ ] 将本地图片上传到图片托管网站获取网络图片地址
-- [ ] 将原 markdown 中本地图片地址替换为网络图片地址
+- [X] 将本地图片上传到图片托管网站获取网络图片地址
+- [X] 将原 markdown 中本地图片地址替换为网络图片地址
 
 # Usage
 
-将你想要的处理的 markdown 文件放在当前目录，输入：
+1. 进入 https://smms.app/，按照指引注册并登陆账号，点击右上角 `User` -> `Dashboard` -> `API Token` -> `Generate Secret Token`，然后复制你的 Secret Token
 
-```bash
-$ python -m Markdown-Image-Upload your-markdown-file.md
-```
+2. 将你想要的处理的 markdown 文件放在当前目录，输入：
 
-即可
+   ```bash
+   $ python -m Markdown-Image-Upload your-markdown-file.md "your-Secret-Token"
+   ```
